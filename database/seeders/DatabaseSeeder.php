e5336cb6d15a62fa7102ef4d49df99f89860651f
Otto Szarata <szystems@hotmail.com>
2026-04-30 18:09:49 -0700

    feat(seed): add super admin user to DatabaseSeeder
    admin@controclinic.com / password (is_super_admin = true)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +21,19 @@ class DatabaseSeeder extends Seeder
 
         // Crear planes
         $this->call(PlansSeeder::class);
+
+        // Super administrador de la plataforma
+        User::firstOrCreate(
+            ['email' => 'admin@controclinic.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'),
+                'is_super_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $this->command->info('✅ Super Admin: admin@controclinic.com / password');
 
         // Crear clínica demo para desarrollo
         $this->call(DemoClinicSeeder::class);
